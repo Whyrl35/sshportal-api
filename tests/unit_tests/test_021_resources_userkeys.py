8b3e742b1520d5ff0e4af21fd2b9ac0d7dd92b25
Ludovic Houdayer <ludovic.houdayer@fr.clara.net>
2020-04-09 21:14:10 +0200

    basic userkeys have no data
    Signed-off-by: Ludovic Houdayer <ludovic.houdayer@fr.clara.net>

--- tests/unit_tests/test_021_resources_userkeys.py
+++ tests/unit_tests/test_021_resources_userkeys.py
@@ -6,8 +6,6 @@ def test_userkeys(client, access_token):
     res = client.get(url_for('userkeys'), headers={'authorization': "Bearer {token}".format(token=token)})
     assert res.status_code == 200
     assert res.json[0]['id'] == 1
-    assert len(res.json[0]['key']) > 1
-    assert len(res.json[0]['authorized_key']) > 1
     assert res.json[0]['comment'] == "created by sshportal"
 
 
@@ -16,6 +14,4 @@ def test_userkey(client, access_token):
     res = client.get(url_for('userkey', id=1), headers={'authorization': "Bearer {token}".format(token=token)})
     assert res.status_code == 200
     assert res.json['id'] == 1
-    assert len(res.json['key']) > 1
-    assert len(res.json['authorized_key']) > 1
     assert res.json['comment'] == "created by sshportal"
